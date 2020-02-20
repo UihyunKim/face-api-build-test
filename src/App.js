07dc938d19d4e6d10fd7aed5b1a6a617f460f6dd
uihyunKIM <stringee@gmail.com>
2020-02-20 14:34:21 +0100

    face-api-build-test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import * as faceapi from "face-api.js";
+import Webcam from "react-webcam";
+import { MODEL_URL } from "./helper";
+import "./styles/main.scss";
 
 function App() {
+  const width = 640;
+  const height = 320;
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const loadModels = async () => {
+    console.log("try loadTinyFaceDetectorModel...");
+    await faceapi.loadTinyFaceDetectorModel(MODEL_URL);
+    console.log("try loadFaceLandmarkModel...");
+    await faceapi.loadFaceLandmarkModel(MODEL_URL);
+    trackingLoop();
+  };
+
+  useEffect(() => {
+    loadModels();
+  });
+
+  /**
+   * LOOP RENDER
+   */
+  const trackingLoop = async () => {
+    const faceFound = await faceapi
+      .detectSingleFace(
+        webcamRef.current.video,
+        new faceapi.TinyFaceDetectorOptions()
+      )
+      .withFaceLandmarks();
+
+    const dims = faceapi.matchDimensions(
+      canvasRef.current,
+      webcamRef.current.video,
+      true
+    );
+
+    if (faceFound) {
+      const resizedResults = faceapi.resizeResults(faceFound, dims);
+      faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedResults);
+    }
+    setTimeout(() => trackingLoop(), 0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="camera" className="position-relative" style={{ width, height }}>
+      <Webcam
+        audio={false}
+        mirrored={true}
+        ref={webcamRef}
+        className="position-absolute"
+        style={{
+          top: 0,
+          left: 0,
+          maxWidth: "100%",
+          maxHeight: "100%"
+        }}
+      />
+      <canvas
+        className="position-absolute"
+        ref={canvasRef}
+        width="640px"
+        height="480px"
+        style={{ top: 0, left: 0, transform: "scaleX(-1)" }}
+      ></canvas>
     </div>
   );
 }
